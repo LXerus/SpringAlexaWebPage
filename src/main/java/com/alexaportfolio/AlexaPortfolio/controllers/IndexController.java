34de2a547d35436ee23026c94507ddb7690032e4
Cristhian Franco <crodfranco@gmail.com>
2020-02-21 09:36:04 -0600

    Email form validation

--- src/main/java/com/alexaportfolio/AlexaPortfolio/controllers/IndexController.java
+++ src/main/java/com/alexaportfolio/AlexaPortfolio/controllers/IndexController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import javax.validation.Valid;
 
 @Controller
@@ -20,16 +20,17 @@ public class IndexController implements WebMvcConfigurer {
         registry.addViewController("/emailsent").setViewName("emailsent");
     }
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String showHome(EmailForm emailForm){
         return "index";
     }
 
-    @PostMapping("/")
+    @PostMapping("/home")
     public String submitEmail(@Valid EmailForm emailForm, BindingResult bindingResult){
-        if (bindingResult.hasErrors()){
+
+        if (bindingResult.hasErrors()) {
             return "index";
         }
-        return "redirect:/emailsent";
+        return "emailsent";
     }
 }
